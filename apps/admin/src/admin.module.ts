7dd91787f5323691379a3448835475276fd88da0
WarrenLee19 <lidipo@qq.com>
2023-01-30 17:46:57 +0800

    user model crud

--- apps/admin/src/admin.module.ts
+++ apps/admin/src/admin.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import {DbModule} from "@libs/db";
-import { UsersController } from './users/users.controller
+import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module';
+import { EpisodesModule } from './episodes/episodes.module';
 
 @Module({
   imports: [
       DbModule,
-      UsersModule
+      UsersModule,
+      CoursesModule,
+      EpisodesModule
   ],
   controllers: [AdminController, UsersController],
   providers: [AdminService],
--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
+  const options = new DocumentBuilder()
+      .setTitle('全栈学习-后端管理API').setDescription('供后端调用API').setVersion('1.1').addTag('1').build();
+  const document = SwaggerModule.createDocument(app,options)
+  SwaggerModule.setup('api-docs', app, document)
   await app.listen(3000);
+  console.log('http://localhost:3000/api-docs')
 }
 bootstrap();
